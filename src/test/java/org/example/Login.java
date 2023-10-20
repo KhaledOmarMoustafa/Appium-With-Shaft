ab14deaf586266d7d8cb61255148848a8b6566e7
bugzzer <khaled_omar2011@hotmail.com>
2023-10-20 18:32:02 +0300

    New Changes

--- src/test/java/org/example/Login.java
+++ src/test/java/org/example/Login.java
@@ -14,31 +14,9 @@ public class Login {
         By loginwithInvalidUser = By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]/android.widget.TextView\n");
         PreRequisites.driver.element().click(loginwithInvalidUser);
 
-       /* String errorMessage = "Sorry, this user has been locked out.";
-        By errorLocator = By.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView\n");
-        String errorMessageLocator = PreRequisites.driver.element().getText(errorLocator);*/
-
 
         Thread.sleep(2000);
 
-        if (PreRequisites.driver.element().isElementDisplayed(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView\n"))) {
-            PreRequisites.appiumDriver.findElement(invalidUser).clear();
-            String userField = PreRequisites.appiumDriver.findElement(invalidUser).getText();
-            System.out.println(userField);
 
-            PreRequisites.appiumDriver.findElement(invalidUserpassWord).clear();
-
-            Thread.sleep(1000);
-
-
-            PreRequisites.driver.element().type(invalidUser, userName);
-
-
-            //By passWord = By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]\n");
-            PreRequisites.driver.element().type(invalidUserpassWord, pword);
-
-            By login = By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]/android.widget.TextView\n");
-            PreRequisites.driver.element().click(login);
-        }
     }
 }
